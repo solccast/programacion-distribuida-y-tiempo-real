33705f3b2b96b2ca909356586f0ccb2d4dc3fd86
Sol Angela Chilaca Castro <solaccastro@gmail.com>
2025-08-29 18:04:08 -0300

    add codigo original y modificado

---
+++ practica/code/client.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>  // Para strlen y otras funciones de cadenas
+#include <unistd.h>  // Para read, write y otras funciones de E/S
+#include <stdlib.h>  // Para exit y otras funciones estándar
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(1);
+    }
+
+    // TOMA EL NÚMERO DE PUERTO DE LOS ARGUMENTOS
+    portno = atoi(argv[2]);
+	
+    // CREA EL FILE DESCRIPTOR DEL SOCKET PARA LA CONEXIÓN
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // AF_INET - FAMILIA DEL PROTOCOLO - IPV4 PROTOCOLS INTERNET
+    // SOCK_STREAM - TIPO DE SOCKET 
+	
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+	
+    // TOMA LA DIRECCIÓN DEL SERVIDOR DE LOS ARGUMENTOS
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    // LIMPIA LA ESTRUCTURA serv_addr
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+	
+    // COPIA LA DIRECCIÓN IP Y EL PUERTO DEL SERVIDOR A LA ESTRUCTURA DEL SOCKET
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+	
+    // DESCRIPTOR - DIRECCIÓN - TAMAÑO DIRECCIÓN
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+
+    printf("Please enter the message: ");
+    bzero(buffer, 256);
+    fgets(buffer, 255, stdin);
+
+    // ENVÍA UN MENSAJE AL SOCKET
+    n = write(sockfd, buffer, strlen(buffer));
+    if (n < 0) 
+         error("ERROR writing to socket");
+
+    bzero(buffer, 256);
+	
+    // ESPERA RECIBIR UNA RESPUESTA
+    n = read(sockfd, buffer, 255);
+    if (n < 0) 
+         error("ERROR reading from socket");
+    
+    printf("%s\n", buffer);
+
+    // CIERRA EL SOCKET
+    close(sockfd);
+    return 0;
+}
