04355362a8e58041acc67ae35481ec2a6f95702e
Franco Kumichel <francokumichel1996@gmail.com>
2025-09-26 06:59:14 -0300

    Modifico ej2 para medir tiempos tiempos acordes al enunciado y además agrego funcion para solucionar oscilaciones indeseadas en el write

--- practica/practica2/codigo/ej2/client_prueba.c
+++ practica/practica2/codigo/ej2/client_prueba.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <netdb.h>
 #include <string.h>
 #include <unistd.h>
@@ -31,7 +32,7 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr;
     struct hostent *server;
     double t0, t1, t2, t3, tiempo_write, tiempo_read, tiempo_acumulado = 0.0, elapsed; 
-    double tiempos[CANTIDAD_EXPERIMENTO];
+    double tiempos_write[CANTIDAD_EXPERIMENTO], tiempos_read[CANTIDAD_EXPERIMENTO];
 
     if (argc < 4) {
        fprintf(stderr,"usage %s hostname port cantidad_bytes\n", argv[0]);
@@ -44,6 +45,9 @@ int main(int argc, char *argv[])
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
         error("ERROR opening socket");
+
+    int flag = 1;
+    setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
 
     /*Se toma la dirección del servidor de los argumentos*/
     server = gethostbyname(argv[1]); 
@@ -75,16 +79,20 @@ int main(int argc, char *argv[])
     for (int i = 0; i < CANTIDAD_EXPERIMENTO; i++){
         t0 = dwalltime(); 
         n = write(sockfd, &buffer[0], cantidad_bytes);
+        t1 = dwalltime()-t0;
+        tiempos_write[i] = t1 * 1000; // Convertir a milisegundos
         // Esperar respuesta del servidor
+        t0 = dwalltime();
         n = read(sockfd, respuesta, sizeof(respuesta)-1);
-        t1 = dwalltime();
-        tiempos[i] = (t1 - t0) * 1000; // Convertir a milisegundos
+        t1 = dwalltime()-t0;
+        tiempos_read[i] = t1 * 1000; // Convertir a milisegundos
         respuesta[n] = '\0';
-        printf("| CLIENTE:: | %d | %f | %f | %s |\n", cantidad_bytes, tiempos[i], tiempos[i]/2.0, respuesta);
+        printf("| CLIENTE:: | %d | %f | %f | %s |\n", cantidad_bytes, tiempos_write[i], tiempos_read[i], respuesta);
     }
-    
-    double promedio = calcularPromedio(tiempos, CANTIDAD_EXPERIMENTO);
-    printf("Promedio de tiempo para %d bytes: %f milisegundos\n", cantidad_bytes, promedio);
+
+    double promedio_write = calcularPromedio(tiempos_write, CANTIDAD_EXPERIMENTO);
+    double promedio_read = calcularPromedio(tiempos_read, CANTIDAD_EXPERIMENTO);
+    printf("Promedio de tiempo write y read para %d bytes: %f milisegundos, %f milisegundos\n", cantidad_bytes, promedio_write, promedio_read);
 
     close(sockfd);
     free(buffer);
--- practica/practica2/codigo/ej2/server_prueba.c
+++ practica/practica2/codigo/ej2/server_prueba.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <sys/time.h>
 
 double dwalltime(); 
@@ -45,6 +46,9 @@ int main(int argc, char *argv[])
     int optval = 1;
     setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
 
+    int flag = 1;
+    setsockopt(sockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
+
     if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR on binding");
 
@@ -54,6 +58,7 @@ int main(int argc, char *argv[])
     if (newsockfd < 0)
         error("ERROR on accept");
     
+    setsockopt(newsockfd, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));    
 
     for (int i = 0; i < CANTIDAD_EXPERIMENTO; i++){
         /*Reserva de memoria ---------*/
